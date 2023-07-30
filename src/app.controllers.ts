cdef3bfa420bd5e5565829eae2b2f5fd6cbe7e15
Nicole <lancasternj@gmail.com>
2023-07-30 17:16:13 +0100

    implementing correct time and date response for the 'happy path' API call

--- src/app.controllers.ts
+++ src/app.controllers.ts
@@ -2,11 +2,28 @@ import Express from "express";
 
 export const getBasicHtml = (
   request: Express.Request,
-  response: Express.Response
+  response: Express.Response,
 ) => {
   try {
     return response.status(200).sendFile(`${__dirname}/views/index.html`);
   } catch (err) {
-    console.error(err);
+    return console.error(err);
+  }
+};
+
+export const getDate = (
+  request: Express.Request,
+  response: Express.Response,
+) => {
+  const { date } = request.params;
+  const dateObj = new Date(date);
+  const unixDate = dateObj.getTime();
+  const utcDate = dateObj.toUTCString();
+  const dateAndTimeStamp = { unix: unixDate, utc: utcDate };
+
+  try {
+    return response.status(200).send(dateAndTimeStamp);
+  } catch (err) {
+    return console.error(err);
   }
 };
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { getBasicHtml } from "./app.controllers";
+import { getBasicHtml, getDate } from "./app.controllers";
 export const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
-
 app.use(express.static("public"));
 
 app.get("/api", getBasicHtml);
+app.get("/api/:date?", getDate);
