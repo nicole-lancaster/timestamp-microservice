823f69342f73f9f3cd99aefac0f23c8f3004ddf9
Nicole <lancasternj@gmail.com>
2023-08-09 17:53:09 +0100

    completing endpoint to get current date if user doesn't input date

--- src/app.controllers.ts
+++ src/app.controllers.ts
@@ -16,13 +16,43 @@ export const getDate = (
   response: Express.Response,
 ) => {
   const { date } = request.params;
-  const dateObj = new Date(date);
-  const unixDate = dateObj.getTime();
-  const utcDate = dateObj.toUTCString();
-  const dateAndTimeStamp = { unix: unixDate, utc: utcDate };
+
+  const unixRegex = /^[0-9]+$/;
+  const dateIsNumberFormat = unixRegex.test(`${date}`);
+  let timeStamp;
+
+  if (!dateIsNumberFormat) {
+    const unixTimestamp = Date.parse(date);
+    const utcDate = new Date(unixTimestamp).toUTCString();
+
+    unixTimestamp
+      ? (timeStamp = { unix: unixTimestamp, utc: utcDate })
+      : (timeStamp = { error: "Invalid Date" });
+  } else {
+    const unixTimestamp = parseInt(date);
+    const actualDate = new Date(unixTimestamp);
+    const utcDate = actualDate.toUTCString();
+
+    timeStamp = { unix: unixTimestamp, utc: utcDate };
+  }
 
   try {
-    return response.status(200).send(dateAndTimeStamp);
+    return response.status(200).send(timeStamp);
+  } catch (err) {
+    return console.error(err);
+  }
+};
+
+export const getCurrentDate = (
+  request: Express.Request,
+  response: Express.Response,
+) => {
+  const currentTimeStamp = {
+    unix: Date.now(),
+    utc: new Date(Date.now()).toUTCString(),
+  };
+  try {
+    return response.status(200).send(currentTimeStamp);
   } catch (err) {
     return console.error(err);
   }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
-import { getBasicHtml, getDate } from "./app.controllers";
+import { getBasicHtml, getCurrentDate, getDate } from "./app.controllers";
 export const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
 
-app.get("/api", getBasicHtml);
-app.get("/api/:date?", getDate);
+app.get("/", getBasicHtml);
+app.get("/api", getCurrentDate);
+app.get("/api/:date", getDate);
